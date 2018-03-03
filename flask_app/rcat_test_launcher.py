cd910c112c307e2dc0f2872514f6dd2ee87961a7
Florian Barth <florianbarth@me.com>
2018-03-03 05:50:36 +0100

    test parameter

--- flask_app/rcat_test_launcher.py
+++ flask_app/rcat_test_launcher.py
@@ -5,7 +5,7 @@ from rcat.rcat_main import rcat
 #rcat().main_PDF(text_file="/Users/Florian 1/Applications/Source_Tree/web-rcat/flask_app/data/CRETA_internal/Werther/1 Goethe_Die_Leiden_des_jungen_Werthers_1774.txt", character_file="/Users/Florian 1/Applications/Source_Tree/web-rcat/flask_app/data/CRETA_internal/Werther/2 Goethe_Werther_74_characters.txt",
 rcat().main_PDF(text_file="./data/CRETA_internal/Iwein/oneword/1_iwein_text.txt", character_file="./data/CRETA_internal/Iwein/oneword/2_iwein_characters.txt",
 #rcat().main_PDF(text_file="/Users/Florian/Applications/SourceTree/web-rcat/flask_app/data/CRETA_internal/Got/000001.txt", character_file="/Users/Florian/Applications/SourceTree/web-rcat/flask_app/data/CRETA_internal/Got/got_characters.txt",
-				dist_parameter=[8, 5, 5],
+				dist_parameter=[10, 8, 8],
 				remove_stopwords_in_context="y",
 				segments=5, txt_language="MHG", number_of_wc=3,
 				write_gephi_csv="n",
